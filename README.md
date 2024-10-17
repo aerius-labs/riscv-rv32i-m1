63620aeaf2198d17bdc847d6d8ff86a3bba63d9b
Rahul Ghangas <rahulghangas0@gmail.com>
2024-10-17 20:23:05 +0530

    chore: upload binaries

---
+++ README.md
@@ -0,0 +1,5 @@
+# Riscv rv32i for Apple Silicon
+
+Prebuilt [risc-v toolchain](https://github.com/riscv-collab/riscv-gnu-toolchain) for apple silicon M1 mac. Compiled with `--prefix=/opt/riscv --with-arch=rv32i`
+
+[License](https://github.com/riscv-collab/riscv-gnu-toolchain/blob/master/LICENSE)
---
+++ lib/gcc/riscv32-unknown-elf/11.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/comgu/riscv-gnu-toolchain/build/../riscv-gcc/configure --target=riscv32-unknown-elf --prefix=/opt/riscv --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/opt/riscv/riscv32-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=../../riscv-gcc --disable-multilib --with-abi=ilp32 --with-arch=rv32i --with-tune=rocket 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32" }, { "arch", "rv32i" }, { "tune", "rocket" } };
